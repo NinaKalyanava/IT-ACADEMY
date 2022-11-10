ae0a0fe76e5e31d1205878833d00aed788f94a34
Nina Kalyanava <ninadylevskaya@mail.ru>
2022-11-10 14:36:13 +0300

    add Lesson 7 Task 1

---
+++ src/Lesson7/House.java
@@ -0,0 +1,27 @@
+package Lesson7;
+
+public class House {
+    int length;
+    int width;
+    String name;
+
+
+    House(int length, int width, String name) {
+        //инициализация переменных кот объявили выше   конструктор
+        this.length = length;
+        this.width = width;
+        this.name = name;
+    }
+
+    void stay() {
+        System.out.println("Stay void ");
+
+    }
+
+    int stay(int length, String newName) {
+        this.length = length;
+        name = newName;
+        System.out.println(this.length + " " + name);
+        return width;
+    }
+}
---
+++ src/homework7/Application.java
@@ -0,0 +1,12 @@
+package homework7;
+
+public class Application {
+    public static void main(String[] args) {
+        Fraction fraction = new Fraction(8, 10);
+        fraction.division();
+        fraction.additionOfFractions(4, 5);
+        fraction.fractionalMultiplication(0.5);
+        fraction.fractionalDivision(0.6);
+    }
+
+}
