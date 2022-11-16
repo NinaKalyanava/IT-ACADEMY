e05cb9fb987fc9a659fd7dc836dab499360e2e31
Nina Kalyanava <ninadylevskaya@mail.ru>
2022-11-16 16:47:54 +0300

    add 4 task (homework 8)

--- src/homeWork8/HomeWork8Task4.java
+++ src/homeWork8/HomeWork8Task4.java
@@ -1,7 +1,21 @@
 package homeWork8;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class HomeWork8Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter email");
+        String email = scanner.nextLine();
 
+        Pattern pattern = Pattern.compile("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}");
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find()) {
+            System.out.println("Good email!  " + email);
+        } else {
+            System.out.println("Email entered incorrectly. Try again.");
+        }
     }
 }
